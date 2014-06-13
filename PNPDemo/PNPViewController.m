a3352117ea470c67debd9b4697954ce81f21476e
Horace Williams <horace.d.williams@gmail.com>
2014-06-13 15:47:19 -0400

    upnp ios demo

--- PNPDemo/PNPViewController.m
+++ PNPDemo/PNPViewController.m
@@ -7,9 +7,14 @@
 //
 
 #import "PNPViewController.h"
+#import "UPnPDB.h"
+#import "UPnPManager.h"
+#import "MediaServerBasicObjectParser.h"
 
-@interface PNPViewController ()
 
+@interface PNPViewController () <UPnPDBObserver>
+@property (nonatomic, strong) NSArray *mDevices;
+@property (nonatomic, strong) NSMutableArray *mPlaylist;
 @end
 
 @implementation PNPViewController
@@ -17,13 +22,69 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    UPnPDB* db = [[UPnPManager GetInstance] DB];
+    self.mDevices = [db rootDevices];
+    NSLog(@"observe DB!");
+    [db addObserver:(UPnPDBObserver *)self];
+    [[[UPnPManager GetInstance] SSDP] searchSSDP];
+    self.mPlaylist = [[NSMutableArray alloc] init];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+//protocol UPnPDBObserver
+-(void)UPnPDBWillUpdate:(UPnPDB*)sender{
+    NSLog(@"UPnPDBWillUpdate %d", [self.mDevices count]);
+}
+
+-(void)UPnPDBUpdated:(UPnPDB*)sender{
+    NSLog(@"UPnPDBUpdated %d", [self.mDevices count]);
+    NSLog(@"devices: %@", self.mDevices);
+    for (BasicUPnPDevice *device in self.mDevices) {
+        NSLog(@"urn is %@", device.urn);
+        if([[device urn] isEqualToString:@"urn:schemas-upnp-org:device:MediaServer:1"]){
+            MediaServer1Device *server = (MediaServer1Device*)device;
+            
+            //pass by reference strings to read output
+            NSMutableString *outResult = [[NSMutableString alloc] init];
+            NSMutableString *outNumberReturned = [[NSMutableString alloc] init];
+            NSMutableString *outTotalMatches = [[NSMutableString alloc] init];
+            NSMutableString *outUpdateID = [[NSMutableString alloc] init];
+
+            
+            [[server contentDirectory] BrowseWithObjectID:@"0"
+                                               BrowseFlag:@"BrowseDirectChildren"
+                                                   Filter:@"*"
+                                            StartingIndex:@"0"
+                                           RequestedCount:@"0"
+                                             SortCriteria:@"+dc:title'"
+                                                OutResult:outResult
+                                        OutNumberReturned:outNumberReturned
+                                          OutTotalMatches:outTotalMatches
+                                              OutUpdateID:outUpdateID];
+            
+            NSData *didl = [outResult dataUsingEncoding:NSUTF8StringEncoding];
+            
+            NSLog(@"preparing to read playlist");
+            
+            [self.mPlaylist removeAllObjects]; //clear playlist so we can add to it
+            MediaServerBasicObjectParser *parser = [[MediaServerBasicObjectParser alloc] initWithMediaObjectArray:self.mPlaylist];
+            [parser parseFromData:didl];
+            NSLog(@"playlist count is %d", [self.mPlaylist count]);
+            NSLog(@"playlist is %@", self.mPlaylist);
+            
+            for (MediaServer1BasicObject *item in self.mPlaylist) {
+                NSLog(@"playlist item title is %@", item.title);
+            }
+        }
+
+    }
+
 }
 
 @end
